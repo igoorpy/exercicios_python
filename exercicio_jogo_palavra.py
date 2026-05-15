fba9b7b9ff4f3a16c85caefe3f58d09d1f22740f
Igor <igoorbrendw@gmail.com>
2026-05-15 14:49:39 -0400

    add import os (cls) para limpar. palavra_formada + numero_tentativas add

--- exercicio_jogo_palavra.py
+++ exercicio_jogo_palavra.py
@@ -1,8 +1,13 @@
-palavra_secreta = 'relógio'
+import os
+
+palavra_secreta = 'relogio'
 letras_acertadas = ''
+numero_tentativas = 0
 
 while True:
     letra_digitada = input('Digite uma letra: ')
+    numero_tentativas += 1
+    print('Tentativas:', numero_tentativas)
 
     if len(letra_digitada) > 1:
         print('Digite apenas uma letra')
@@ -10,11 +15,19 @@ while True:
     if letra_digitada in palavra_secreta:
         letras_acertadas += letra_digitada
         
+    palavra_formada = ''
     for letra_secreta in palavra_secreta:
         if letra_secreta in letras_acertadas:
-            print(letra_secreta)
+            palavra_formada += letra_secreta
         else:
-            print('*')
+           palavra_formada += '*'
 
+    print('Palavra formada:', palavra_formada)
 
+    if palavra_formada == palavra_secreta:
+        os.system('cls')
+        print('Você ganhou!')
+        print('A palavra era', palavra_secreta)
+        print('Tentativas: ', numero_tentativas)
 
+    
