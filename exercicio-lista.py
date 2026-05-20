d3dedeab8448dfe04253519cb140f2e472ce575e
Igor <igoorbrendw@gmail.com>
2026-05-20 16:51:05 -0400

    editado

--- exercicio-lista.py
+++ exercicio-lista.py
@@ -1,34 +1,48 @@
 import os 
 
 lista = []   
+
 while True:
-    print ('Selecione uma opção')
-    opcao = input('[i]nserir [a]pagar [l]istar: ')  
+    print('\nSelecione uma opção')
+    opcao = input('[i]nserir [a]pagar [l]istar: ').strip().lower()
 
     if opcao == 'i':
         os.system('cls')
         valor = input('Valor: ')
         lista.append(valor)
+        print(f'{valor} adicionado')
+
     elif opcao == 'a':   
-        indice_str = input('Escolha o indice para apagar: ')
+        if not lista:
+            print('Lista vazia')
+            continue
+
+        print('\n Lista atual:')
+        for i, valor in enumerate(lista):
+            print(f'  [{i}] {valor}')
+        
+        indice_str = input('\nEscolha o indice para apagar: ')
 
         try:
             indice = int(indice_str)
-            del lista [indice]
+            item_removido = lista[indice]
+            del lista[indice]
+            print(f' "{item_removido}" removido!')
         except ValueError:
-            print('Digite um numero inteiro')
+            print(' Digite um numero inteiro')
         except IndexError:
-            print('Indice nao existe na lista')
+            print(' Indice nao existe na lista')
         except Exception:
-            print('Error')
+            print(' Erro')
 
     elif opcao == 'l':
         os.system('cls')
 
         if len(lista) == 0:
             print('Nada para listar')  
-
-        for i, valor in enumerate(lista):
-            print(i, valor)
+        else:
+            for i, valor in enumerate(lista):
+                print(i, valor)
+                
     else:
         print('Opção inválida')
