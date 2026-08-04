10575346249d3caba5588ca0717b7880b9d07c7e
Igor <igoorbrendw@gmail.com>
2026-08-04 02:42:48 -0400

    Ex. validação de senha atual.1

---
+++ Exercícios -1/ex-validador-de-senhas-2.py
@@ -0,0 +1,11 @@
+# while - if/else
+senha_validador = "python123"
+nome = input("Qual o seu nome?: ")
+senha = input("Informe a sua senha: ")
+
+
+if senha == senha_validador:
+    print(f"Seja Bem-vindo, {nome}!")
+
+else:
+    ("Senha incorrreta!")
