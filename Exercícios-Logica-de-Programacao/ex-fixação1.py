1eb0e169f1d8045b83eed417013ee3d2fe9cefbd
Igor <igoorbrendw@gmail.com>
2026-08-06 16:27:51 -0400

    adição do not para senha.strip()

--- Exercícios-Logica-de-Programacao/ex-fixação1.py
+++ Exercícios-Logica-de-Programacao/ex-fixação1.py
@@ -5,6 +5,10 @@ tentativas = 0
 while tentativas < 3:
     senha = input('Informe a sua senha: ')
 
+    if not senha.strip():
+        print("Você não digitou nada! Digite uma senha válida.")
+        continue 
+
     if senha == senha_correta:
         print(f"Bem vindo {nome}, acesso liberado!")
         break  
