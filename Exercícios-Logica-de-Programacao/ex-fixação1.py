cdde47243b99ac7887f4dde71ac8676369161745
Igor <igoorbrendw@gmail.com>
2026-08-06 16:23:35 -0400

    Exercicio de fixacao niel-facil

---
+++ Exercícios-Logica-de-Programacao/ex-fixação1.py
@@ -0,0 +1,16 @@
+senha_correta = 'igor123'
+nome = input('Qual o seu nome?: ')
+tentativas = 0 
+
+while tentativas < 3:
+    senha = input('Informe a sua senha: ')
+
+    if senha == senha_correta:
+        print(f"Bem vindo {nome}, acesso liberado!")
+        break  
+    else:
+        print("Acesso negado! Tente novamente.")
+        tentativas += 1
+
+if tentativas == 3:
+    print('Acesso Bloqueado!')
