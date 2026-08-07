e4219c74a52106af35ec7bb1fd2caadf040914a0
Igor <igoorbrendw@gmail.com>
2026-08-07 15:26:43 -0400

    Exercicios de funções simples e identificação de parametros

---
+++ Exercícios-2-Funcoes-Dicionarios-Modulos-/ex-funcoes.py
@@ -0,0 +1,26 @@
+def multiplicar(*args):
+    total = 1
+    for numero in args:
+        total *= numero
+    return total
+
+multiplicação = multiplicar(10, 2, 3, 4, 5)
+print(multiplicação)
+
+
+def par_impar(numero):
+    multiplo_de_dois = numero % 2 == 0
+
+    if multiplo_de_dois:
+        return f'{numero} é par'
+    return f'{numero} é ímpar'
+
+
+outro_par_impar = par_impar
+dois_e_par = outro_par_impar(2)
+print(dois_e_par)
+print(par_impar(3))
+print(par_impar(15))
+print(par_impar(16))
+
+print(par_impar is outro_par_impar)
