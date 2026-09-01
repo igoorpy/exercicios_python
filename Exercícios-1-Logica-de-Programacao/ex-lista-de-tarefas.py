3080931535cadfa74f07012dfd66a28bfa0a713d
Igor <igoorbrendw@gmail.com>
2026-09-01 16:59:26 -0400

    adiciona exercicio lista de tarefas e organiza arquivos

---
+++ Exercícios-1-Logica-de-Programacao/ex-lista-de-tarefas.py
@@ -0,0 +1,12 @@
+# 1. Crio uma lista vazia para guardar minhas tarefas
+tarefas = []
+
+# 2. Pergunto uma tarefa para o usuário
+nova_tarefa = input("Digite uma tarefa para adicionar: ")
+
+# 3. Coloco essa nova tarefa dentro da minha lista (append adiciona no final)
+tarefas.append(nova_tarefa)
+
+# 4. Mostro na tela como a lista ficou
+print("Minhas tarefas até agora:")
+print(tarefas)
